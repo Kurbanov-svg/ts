98682039feeebd7253e153d8eceed9f94e30fa58
Kurbanov-svg <asinn975@gmail.com>
2025-08-05 14:55:38 +0600

    .

--- src/components/crud/MessageList/MessageList.tsx
+++ src/components/crud/MessageList/MessageList.tsx
@@ -14,13 +14,23 @@ const MessageList = () => {
       {dataList && dataList?.length > 0 ? (
         dataList.map(({ _id, author, message, datetime }) => (
           <div key={_id} className="mb-3 border-bottom pb-2">
-            <div className="d-flex justify-content-between">
-              <strong>{author}</strong>
-              <small className="text-muted">
-                {new Date(datetime).toLocaleTimeString()}
+            <div className="d-flex justify-content-between align-items-center">
+              <strong>Author:{author}</strong>
+              <small
+                style={{
+                  color: "#ffffff",
+                  fontStyle: "italic",
+                  fontSize: "0.75rem",
+                  marginLeft: "10px",
+                }}
+              >
+                {new Date(datetime).toLocaleTimeString([], {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}
               </small>
             </div>
-            <div>{message}</div>
+            <div>Message: {message}</div>
           </div>
         ))
       ) : (
